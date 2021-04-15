544381e9151505f4437ff5075a19761b31ece848
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-04-16 02:51:48 +0530

    Add: sockets' callback index file

--- socket/index.js
+++ socket/index.js
@@ -1,8 +1,14 @@
-function main(io) {
-	return () => io(server);
+// const io = require('../server');
+const updateText = require('./updateText.emit');
+function connect(io, socket, DATA) {
+	const id = DATA._id;
+	io.emit('meta', { DATA, conn: io.engine.clientsCount });
+	socket.on('update-text', (value, callback) =>
+		updateText(socket, value, id, callback)
+	);
+	socket.on('disconnect', client => {
+		io.emit('meta', { DATA, conn: io.engine.clientsCount });
+	});
 }
 
-function io(io) {
-	console.log(io);
-}
-export default main;
+module.exports = connect;
