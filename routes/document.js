89fffb86f0e0f2462e29e912f2f4019c4a4725a3
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-04-16 02:51:16 +0530

    Fix: header already sendt exception(Async)

--- routes/document.js
+++ routes/document.js
@@ -1,23 +1,26 @@
 const Router = require('express').Router();
+const io = require('../server');
+const connect = require('../socket');
 const Doc = require('../database/model/document');
 const BASE_DIR = __dirname + '/../html';
 Router.route('/:id')
 	.get((req, res) => {
-		console.log('Get request');
+		// console.log('Get request');
 		const id = req.params.id;
 		Doc.findById(id)
-			.then(response => res.render(BASE_DIR + '/document', { response }))
-			.catch(error => res.render(BASE_DIR + '/error', { error }));
+			.then(response => {
+				res.render(BASE_DIR + '/document');
+				io.on('connection', socket => connect(io, socket, response));
+			})
+			.catch(error => res.render(BASE_DIR + '/error'));
 	})
 	.delete((req, res) => {
 		const id = req.params.id;
-		console.log('del');
+		console.log('delete request');
 		Doc.findByIdAndDelete(id)
 			.then(response => {
-				console.log(response);
 				res.json({ status: 'SUCCESS', response });
 			})
 			.catch(er => res.json({ status: 'ERROR', response }));
 	});
-
 module.exports = Router;
