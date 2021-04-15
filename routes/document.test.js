44014b485aed82719a2f06e78689c49db07b08f0
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-04-16 02:53:49 +0530

    remove test file

--- routes/document.test.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express').Router();
-const Doc = require('../database/model/document');
-
-Router.route('/').get((req, res) => {
-	Doc.find().then(response => res.json(response));
-});
-
-module.exports = Router;
